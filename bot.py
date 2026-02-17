d1a9869aa3d3cec3f2ea9266c9934cacf16b9cd1
flamewsg-oss <flamewsg@gmail.com>
2026-02-17 13:47:24 +0530

    Update BOT_TOKEN with hardcoded value
    Replaced environment variable for BOT_TOKEN with a hardcoded token.

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8480096137:AAEG6mLJkerqrDeWIRSXC3C1Bedoo3HjHLk")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot is working 🚀")
